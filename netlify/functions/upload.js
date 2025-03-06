36079a04da9d8d802e935e73c623853fbdbace65
barthaasbroek <bart@phoenixsite.nl>
2025-03-06 08:36:04 +0100

    fix upload

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -1,5 +1,4 @@
 import sharp from "sharp";
-import axios from "axios";
 import FormData from "form-data";
 import Airtable from 'airtable';
 
@@ -28,31 +27,35 @@ export const handler = async (event) => {
       .toBuffer();
 
     const formData = new FormData();
-    formData.append("image", compressedImage.toString("base64"));
-    formData.append("key", process.env.IMGBB_API_KEY);
+    // formData.append("image", compressedImage.toString("base64"));
+    // formData.append("key", process.env.IMGBB_API_KEY);
 
-    const response = await axios.post(
-      "https://api.imgbb.com/1/upload",
-      formData,
-      {
-        headers: formData.getHeaders(),
-      }
-    );
+    // const response = await axios.post(
+    //   "https://api.imgbb.com/1/upload",
+    //   formData,
+    //   {
+    //     headers: formData.getHeaders(),
+    //   }
+    // );
 
-    if (response.data.success) {
+    const response = await fetch("https://api.imgbb.com/1/upload?key=" + process.env.IMGBB_API_KEY, {
+        method: "POST",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams({ image: compressedImage.toString("base64") }),
+      });
+
+      const data = await response.json();
+
+    if (data.success) {
       const result = {
-        url: response.data.data.url, // Geef de URL van de geüploade afbeelding terug
+        url: data.data.url, // Geef de URL van de geüploade afbeelding terug
       };
 
       const imageUrl = result.url;
 
-      const updatedData = {
-        "image": imageUrl,
-      };
-
       const recordId = body.recordId;
 
-        await base(AIRTABLE_TABLE_NAME).update(recordId, updatedData, (err) => {
+        await base(AIRTABLE_TABLE_NAME).update(recordId, {image: imageUrl}, (err) => {
         if (err) {
           console.error('Fout bij het bijwerken van het record:', err);
           return;
@@ -64,7 +67,7 @@ export const handler = async (event) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(result), // Geef de URL van de geüploade afbeelding terug
+        body: JSON.stringify(result),
       };
     } else {
       return {
